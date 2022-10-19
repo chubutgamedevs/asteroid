687599d09809395cabda3db450bf57707b362cc5
Matutee <matiascasteglione2002@gmail.com>
2022-10-19 20:06:10 -0300

    Update test2.pas

--- test2.pas
+++ test2.pas
@@ -1,4 +1,4 @@
-Program test;
+Program test2;
 {$UNITPATH ./units}
 
 Uses sdl2, sdl2_image;
@@ -15,55 +15,66 @@ Var
     rectangulo :   TSDL_Rect;
     teclado :   PUInt8;
     running :   Boolean =   True;
-    velocidad :   Real =   0;
+    velocidadx,velocidady :   LongInt;
     aceleracion :    Real =   0.2;
+    vel :   integer =   1;
     maxVelocidad :   Integer =   8;
     ventanaW :   Integer =   800;
     ventanaH :   Integer =   600;
     evento :   PSDL_Event;
 
+Function mover(vx,vy : LongInt):   TSDL_Rect;
+
+Var 
+    rect :   PSDL_Rect;
+Begin
+    rectangulo.x := rectangulo.x + vx;
+    rectangulo.y := rectangulo.y + vy;
+
+    If (rectangulo.x > ventanaW) Then
+        rectangulo.x := -50
+    Else If (rectangulo.x < -50) Then
+        rectangulo.x := ventanaW
+    Else If (rectangulo.y > ventanaH) Then
+        rectangulo.y := -50
+    Else If (rectangulo.y < -50) Then
+        rectangulo.y := ventanaH;
+
+    velocidadx := vx;
+    velocidady := vy;
+    mover := rectangulo;
+End;
+
+
 Function mando(teclado: PUInt8; rectangulo : TSDL_Rect) :   TSDL_Rect;
 Begin
-    If velocidad > maxVelocidad Then
-        velocidad := maxVelocidad;
+    If vel > maxVelocidad Then
+        vel := maxVelocidad;
+
     If (teclado[SDL_SCANCODE_W] = 1) Or (teclado[SDL_SCANCODE_UP] = 1) Then
+        velocidady := velocidady - vel
+    Else If (teclado[SDL_SCANCODE_A] = 1) Or (teclado[SDL_SCANCODE_LEFT] = 1) Then
+        velocidadx := velocidadx - vel
+    Else If (teclado[SDL_SCANCODE_S] = 1) Or (teclado[SDL_SCANCODE_DOWN] = 1) Then
+        velocidady := velocidady + vel
+    Else If (teclado[SDL_SCANCODE_D] = 1) Or (teclado[SDL_SCANCODE_RIGHT] = 1) Then
+        velocidadx := velocidadx + vel
+    Else
         Begin
-            velocidad := velocidad + aceleracion;
-            rectangulo.y := Round(rectangulo.y - velocidad);
-        End
-    else If (teclado[SDL_SCANCODE_A] = 1) Or (teclado[SDL_SCANCODE_LEFT] = 1) Then
-        Begin
-            velocidad := velocidad + aceleracion;
-            rectangulo.x := Round(rectangulo.x - velocidad);
-        End
-    else If (teclado[SDL_SCANCODE_S] = 1) Or (teclado[SDL_SCANCODE_DOWN] = 1) Then
-        Begin
-            velocidad := velocidad + aceleracion;
-            rectangulo.y := Round(rectangulo.y + velocidad);
-        End
-    else If (teclado[SDL_SCANCODE_D] = 1) Or (teclado[SDL_SCANCODE_RIGHT] = 1) Then
-        Begin
-            velocidad := velocidad + aceleracion;
-            rectangulo.x := Round(rectangulo.x + velocidad);
+            If velocidadx > 0 Then
+                velocidadx := velocidadx - vel
+            Else If velocidadx < 0 Then
+                velocidadx := velocidadx + vel;
+            If velocidady > 0 Then
+                velocidady := velocidady - vel
+            Else If velocidady < 0 Then
+                velocidady := velocidady + vel;
         End;
-    While (evento^.type_ = SDL_KEYUP) And (velocidad > 0) Do
-        Begin
-            velocidad := velocidad - aceleracion;
-        End;
-    
-    if (rectangulo.x > ventanaW) then
-        rectangulo.x := -50
-    else if (rectangulo.x < -50) then
-        rectangulo.x := ventanaW
-    else if (rectangulo.y > ventanaH) then
-        rectangulo.y := -50
-    else if (rectangulo.y < -50) then
-        rectangulo.y := ventanaH;
 
     If (teclado[SDL_SCANCODE_ESCAPE] = 1) Or (evento^.window.event = SDL_WINDOWEVENT_CLOSE) Then
         running := False;
 
-    mando := rectangulo;
+    mando := mover(velocidadx,velocidady);
 End;
 
 Procedure salir;
@@ -107,8 +118,6 @@ Begin
             teclado := SDL_GetKeyboardState(Nil);
             rectangulo := mando(teclado, rectangulo);
 
-            
-
             // grey background
             SDL_SetRenderDrawColor(render, 20, 20, 20, SDL_ALPHA_OPAQUE);
             SDL_RenderClear(render);
@@ -117,7 +126,7 @@ Begin
             SDL_SetRenderDrawColor(render, 255, 0, 0, SDL_ALPHA_OPAQUE);
             SDL_RenderFillRect(render, @rectangulo);
             SDL_RenderPresent(render);
-            SDL_Delay(20);
+            SDL_Delay(30);
         End;
     salir;
 End.
